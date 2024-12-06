5b4fdd691942525a9e94c61de5c1cc8c4a05b77c
Martin Portin <Martin@Martins-MacBook.local>
2024-12-06 20:49:27 +0100

    35th commit: Create link to public sector page on home page

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,9 +1,10 @@
 import { Locale } from "@/i18n-config";
 import { getDictionary } from "./dictionaries";
+import Link from "next/link";
 
 export default async function Home({
   params: { lang }
 }: Readonly<{ params: { lang: Locale } }>) {
   const dictionary = await getDictionary(lang);
-  return <div>{dictionary.principals.public}</div>;
+  return <Link href="/public-sector">{dictionary.home.publicSector}</Link>;
 }
