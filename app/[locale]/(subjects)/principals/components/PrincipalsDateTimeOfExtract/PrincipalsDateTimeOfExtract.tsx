44bf960195aa4539daaa741c78bced3336ddcdbf
martinportin <martinportin@gmail.com>
2025-02-15 14:27:02 +0100

    Reorganize project sturcture

--- app/[locale]/(subjects)/principals/components/PrincipalsDateTimeOfExtract/PrincipalsDateTimeOfExtract.tsx
+++
@@ -1,5 +0,0 @@
-export default function PrincipalsDateTimeOfExtract({
-  dateTimeOfExtract
-}: Readonly<{ dateTimeOfExtract: string }>) {
-  return <time dateTime={dateTimeOfExtract}>{dateTimeOfExtract}</time>;
-}
--- app/[locale]/(subjects)/principals/components/PrincipalsDateTimeOfExtract/__test__/PrincipalsDateTimeOfExtract.test.tsx
+++
@@ -1,8 +0,0 @@
-import renderPrincipalsDateTimeOfExtract from './utils/renderers/renderPrincipalsDateTimeOfExtract';
-
-describe('principals date time och extraxt', () => {
-  test('should render the date and time of extract', () => {
-    const { getDateTimeOfExtract } = renderPrincipalsDateTimeOfExtract();
-    expect(getDateTimeOfExtract()).toBeInTheDocument();
-  });
-});
