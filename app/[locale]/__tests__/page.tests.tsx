2c54d3560a5fe39e080bc11ba0922dda5f4a760c
martinportin <martinportin@gmail.com>
2025-05-16 21:55:18 +0200

    I18n

--- app/[locale]/__tests__/page.tests.tsx
+++ app/[locale]/__tests__/page.tests.tsx
@@ -2,11 +2,12 @@ import Home from '../page';
 import { render, screen } from '@testing-library/react';
 
 describe('Home', () => {
-  test('should render a heading with the text "Open Data"', () => {
-    render(<Home />);
+  test('should render a heading with the text "Open Data"', async () => {
+    const home = await Home({ params: Promise.resolve({ locale: 'en' }) });
+    render(home);
     const heading = screen.getByRole('heading', {
       level: 1,
-      name: /Open Data/i
+      name: /header/i
     });
     expect(heading).toBeInTheDocument();
   });
