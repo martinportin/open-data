5b8b9b23d959e079d48b418b3f65b93166b1fc43
martinportin <martinportin@gmail.com>
2025-05-27 22:23:28 +0200

    Search input filter

---
+++ app/[locale]/tests/components/FilterSearchInput.test.tsx
@@ -0,0 +1,56 @@
+import userEvent from '@testing-library/user-event';
+import renderFilterSearchInput from './renderFilterSearchInput';
+
+describe('filter search input', () => {
+  const id = 'mockId';
+  const labelName = /labelName/i;
+  const testValue = 'test';
+  const handleInputChange = jest.fn();
+
+  test('should render a search input', async () => {
+    const { getSearchInput } = await renderFilterSearchInput(
+      labelName,
+      id,
+      '',
+      handleInputChange
+    );
+    expect(getSearchInput()).toBeInTheDocument();
+  });
+
+  test('search input should have the given id', async () => {
+    const { getSearchInput } = await renderFilterSearchInput(
+      labelName,
+      id,
+      '',
+      handleInputChange
+    );
+    expect(getSearchInput()).toHaveAttribute('id', id);
+  });
+
+  test('search input should be empty', async () => {
+    const { getSearchInput } = await renderFilterSearchInput(
+      labelName,
+      id,
+      '',
+      handleInputChange
+    );
+    expect(getSearchInput()).toHaveValue('');
+  });
+
+  test('search input value should be "test"', async () => {
+    const { getSearchInput } = await renderFilterSearchInput(
+      labelName,
+      id,
+      testValue,
+      handleInputChange
+    );
+    expect(getSearchInput()).toHaveValue('test');
+  });
+
+  test('"handleInputChange" should be called when typing in search input', async () => {
+    const { getSearchInput, getHandleInputChange } =
+      await renderFilterSearchInput(labelName, id, '', handleInputChange);
+    userEvent.type(getSearchInput(), 'test');
+    expect(getHandleInputChange()).toHaveBeenCalledTimes(4);
+  });
+});
